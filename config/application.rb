29398ba731112e7ad7bf26f5d7acca7e16e59d35
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-07-02 23:50:46 -0300

    reorder alfabetically

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,9 @@ module HAT
     Dir[File.join('app', 'controllers', '**/*.rb')].each { |file| require File.expand_path(file) }
     Dir[File.join('app', 'models', '**/*.rb')].each { |file| require File.expand_path(file) }
 
+    use Rack::Protection, except: :http_origin
     use Rack::Session::Cookie, secret: SecureRandom.hex(64)
     use Rack::Static, root: 'public', urls: ['/images', '/javascripts', '/stylesheets']
-    use Rack::Protection, except: :http_origin
 
     map('/') { run RootController.new }
   end
