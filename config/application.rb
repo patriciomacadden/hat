54c0e311f4d59f815fed0ce6f8f582ca563e996d
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-07-02 23:50:07 -0300

    fix typo

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ module HAT
     Dir[File.join('app', 'models', '**/*.rb')].each { |file| require File.expand_path(file) }
 
     use Rack::Session::Cookie, secret: SecureRandom.hex(64)
-    use Rack::Static, root: 'public', urls: ['/iamges', '/javascripts', '/stylesheets']
+    use Rack::Static, root: 'public', urls: ['/images', '/javascripts', '/stylesheets']
     use Rack::Protection, except: :http_origin
 
     map('/') { run RootController.new }
