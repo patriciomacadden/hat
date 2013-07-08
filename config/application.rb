0e1ad9bfe44147fb259d76ea89375a17a0163680
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-07-07 23:28:51 -0300

    fix middleware order

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,9 @@ module HAT
     Dir[File.join('app', 'controllers', '**/*.rb')].each { |file| require File.expand_path(file) }
     Dir[File.join('app', 'models', '**/*.rb')].each { |file| require File.expand_path(file) }
 
-    use Rack::Protection, except: :http_origin
     use Rack::Session::Cookie, secret: SecureRandom.hex(64)
+    # must be used after Rack::Session::Cookie
+    use Rack::Protection, except: :http_origin
     use Rack::Static, root: 'public', urls: ['/images', '/javascripts', '/stylesheets']
 
     map('/') { run RootController.new }
