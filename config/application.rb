5ba1779f394e37160023b6744390f7353acb56c1
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-07-22 22:41:46 -0300

    use better_errors only in development

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ module HAT
     Dir[File.join('app', 'controllers', '**/*.rb')].each { |file| require File.expand_path(file) }
     Dir[File.join('app', 'models', '**/*.rb')].each { |file| require File.expand_path(file) }
 
-    use BetterErrors::Middleware
+    use BetterErrors::Middleware if ENV['RACK_ENV'].to_sym == :development
     use Rack::Session::Cookie, secret: SecureRandom.hex(64)
     # must be used after Rack::Session::Cookie
     use Rack::Protection, except: :http_origin
